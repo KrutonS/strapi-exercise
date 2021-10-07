eff52f327e94aa7638069f87d3cf924b65f996a8
KrutonS <krutonpl@gmail.com>
2021-10-07 17:19:35 +0200

    'init'

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'd10c532cabca79f57200f9a3a19871d9'),
+    },
+  },
+});
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '53ad4ec3-c796-46fb-857d-9b653865dab1'
+};
